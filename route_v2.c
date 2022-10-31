c339c0f56d6755c6ebc6fd873562b23184616898
verdale01 <bettsv@mail.gvsu.edu>
2022-10-31 18:32:15 -0400

    updated route_v2.c

--- route_v2.c
+++ route_v2.c
@@ -110,24 +110,25 @@ int main()
     memcpy(&e_h, buf, sizeof(e_h));
 
     char nn[] = "address";
-    // IF we have an incomingICMP echo request/reply
+    // IF we have an incoming ICMP echo request/reply
     if (ntohs(e_h.ether_type) == 0x800)
     {
       /*[Eth header][IP header][ICMP header]*/
 
-      // Eth header
+      // Eth header (type,source mac, dest mac)
+      printf("Type: 0x%03x\n", ntohs(e_h.ether_type));
       printf("ICMP type\n");
       printf("Destination: %s\n", ether_ntoa((struct ether_addr *)&e_h.ether_dhost));
       printf("Source: %s\n", ether_ntoa((struct ether_addr *)&e_h.ether_shost));
-      printf("Type: 0x%03x\n", ntohs(e_h.ether_type));
-      printf("Got a %d byte packet\n", n);
+      //printf("Got a %d byte packet\n", n);
 
-      // printf("size is [%ld]\n",sizeof(e_h));
       // IP header -> If the protocol header in the IP header = 1 this means we have an ICMP request/reply
       memcpy(&ip_h, &buf[sizeof(e_h)], sizeof(ip_h));
       printf("IP Protocol: %d\n", ip_h.protocol);
 
-      // ICMP header
+      // ICMP header (type, code, checksum, identifier, sequence number, optional data)
+      // http://www.tcpipguide.com/free/t_ICMPv4EchoRequestandEchoReplyMessages-2.htm
+      // Offset the buffer data by the size of the ethernet and ip headers
       memcpy(&icmp_h, &buf[sizeof(e_h) + sizeof(ip_h)], sizeof(icmp_h));
       if (icmp_h.icmp_type == 0)
       {
@@ -144,9 +145,8 @@ int main()
         //Place the targets mac address in the data header
         //Send it via the socket to the requester that can then decode byte stream/datagram        
       }
-
-      // Data header
-      memcpy(&data_msg, &buf[sizeof(e_h) + sizeof(ip_h) + sizeof(icmp_h)], sizeof(data_msg));
+      // The ICMP header provides a place for optional data beginning at bit 65 and/or at 8 bytes into the ICMP frame
+      memcpy(&data_msg, &buf[sizeof(e_h) + sizeof(ip_h) + 8], sizeof(data_msg));
       printf("Data message: %s\n", data_msg);
     }
      
@@ -155,27 +155,30 @@ int main()
     {
 
       //[Eth header][ARP header]
-      // ARP frame nes source Hardware type, length, protocol type and IP length
+      // ARP frame needs source Hardware type, length, protocol type and IP length
       // IP/MAC and target IP/MAC
       printf("ARP Type\n");
-      struct arphdr arp_h;
 
-      arp_h.ar_hrd = 1; /* Format of hardware address.  unsigned short int*/
-      arp_h.ar_hln = '6';          /* Length of hardware address.  unsigned char*/
-      arp_h.ar_pro = ntohs(e_h.ether_type);     /* Format of protocol address.  unsigned short int*/
-      arp_h.ar_pln = '4';          /* Length of protocol address.  unsigned char*/
-      
-      // e_h.ether_shost;
-      // ip_h.daddr;
-      // e_h.ether_dhost;
-      // ip_h.daddr; 
+      // struct arphdr arp_h;
+      struct ether_arp full_arp_h;
 
-      // Struct is used to hold source/destination ip/mac
-      struct mac_ip arp;
-      memcpy(&arp.src_mac,&e_h.ether_shost,sizeof(arp.src_mac));	// Setting the source mac
-      memcpy(&arp.src_ip, &ip_h.daddr,sizeof(arp.src_ip));	// Setting the source ip 
-      memcpy(&arp.dst_mac,&e_h.ether_dhost,sizeof(arp.dst_mac));	// Setting the destination mac
-      memcpy(&arp.dst_ip,&ip_h.daddr,sizeof(arp.dst_ip));	// Setting the destination ip
+
+      full_arp_h.ea_hdr.arp_hrd = 1; /* Format of hardware address.  unsigned short int*/
+      full_arp_h.ea_hdr.arp_hln = 6;          /* Length of hardware address.  unsigned char*/
+      full_arp_h.ea_hdr.arp_pro = ntohs(e_h.ether_type);     /* Format of protocol address.  unsigned short int*/
+      full_arp_h.ea_hdr.arp_pln = 4;          /* Length of protocol address.  unsigned char*/
+      full_arp_h.ea_hdr.arp_op = ; // Can be 1 for ARP request and 2 for ARP reply
+      // // e_h.ether_shost;
+      // // ip_h.daddr;
+      // // e_h.ether_dhost;
+      // // ip_h.daddr; 
+
+      // // Struct is used to hold source/destination ip/mac
+      // struct mac_ip arp;
+      // memcpy(&arp.src_mac,&e_h.ether_shost,sizeof(arp.src_mac));	// Setting the source mac
+      // memcpy(&arp.src_ip, &ip_h.daddr,sizeof(arp.src_ip));	// Setting the source ip 
+      // memcpy(&arp.dst_mac,&e_h.ether_dhost,sizeof(arp.dst_mac));	// Setting the destination mac
+      // memcpy(&arp.dst_ip,&ip_h.daddr,sizeof(arp.dst_ip));	// Setting the destination ip
 
       // arp_h.__ar_sha[ETH_ALEN] = e_h.ether_dhost; /* Sender hardware address.  unsigned char*/
       // arp_h.__ar_sip[4];        /* Sender IP address.  unsigned char*/
